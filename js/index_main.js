207b838b29bbaa3343f98dae03a52e53ff9c7451
cshekhar1337 <cshekhar@Chandras-MacBook-Pro.local>
2017-06-16 11:32:46 -0700

    fix: indentation & errors

--- js/index_main.js
+++ js/index_main.js
@@ -2,17 +2,24 @@ var map;
 var defaultIcon;
 var highlightedIcon;
 
+
+// used javascript because in incognito mode jquery is also not loaded. So i have used simple javascript here.
 function mapError() {
 
-    alert("Could not load Google Maps");
+    var headerVal = document.getElementById("app-header");
+    headerVal.innerHTML = "Error:Could not load Google Maps\n" + headerVal.innerHTML;
+    document.getElementById("query-list").style.visibility= "hidden";
 }
 
 //initializes the map
 function initializeMap() {
     "use strict";
-    
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 37.338208, lng: -121.886329},
+        center: {
+            lat: 37.338208,
+            lng: -121.886329
+        },
         zoom: 14,
         mapTypeControl: false
     });
@@ -20,12 +27,12 @@ function initializeMap() {
 
 }
 
-String.prototype.contains = function (other) {
+String.prototype.contains = function(other) {
     return this.indexOf(other) !== -1;
 };
 
 // View Model
-var KnockoutViewModel = function () {
+var KnockoutViewModel = function() {
     var self = this;
 
     function initialize() {
@@ -33,8 +40,7 @@ var KnockoutViewModel = function () {
     }
 
 
-    if (typeof google !== 'object' || typeof google.maps !== 'object') {
-    } else {
+    if (typeof google !== 'object' || typeof google.maps !== 'object') {} else {
         defaultIcon = makeMarkerIcon('800080');
         highlightedIcon = makeMarkerIcon('FFFF00');
         var infoWindow = new google.maps.InfoWindow();
@@ -46,65 +52,64 @@ var KnockoutViewModel = function () {
 
 
     //Creates a object from the data received by foursquare api
-var search_Object = function (dine_spots, map) {
-    var self = this;
-    self.lng = dine_spots.location.lng;
-    self.lat = dine_spots.location.lat;
-    self.formattedPhone = ko.observable(dine_spots.contact.formattedPhone);
-    self.name = ko.observable(dine_spots.name);
-    self.map_location = ko.computed(function () {
-        if (self.lat === 0 || self.lng === 0) {
-            return null;
-        } else {
-            return new google.maps.LatLng(self.lat, self.lng);
-        }
-    });
-    self.formattedAddress = ko.observable(dine_spots.location.formattedAddress);
-    self.marker = (function (dine_spots) {
-        var marker;
+    var SearchObject = function(dine_spots, map) {
+        var self = this;
+        self.lng = dine_spots.location.lng;
+        self.lat = dine_spots.location.lat;
+        self.formattedPhone = ko.observable(dine_spots.contact.formattedPhone);
+        self.name = ko.observable(dine_spots.name);
+        self.map_location = ko.computed(function() {
+            if (self.lat === 0 || self.lng === 0) {
+                return null;
+            } else {
+                return new google.maps.LatLng(self.lat, self.lng);
+            }
+        });
+        self.formattedAddress = ko.observable(dine_spots.location.formattedAddress);
+        self.marker = (function(dine_spots) {
+            var marker;
 
-        if (self.map_location()) {
-            marker = new google.maps.Marker({
-                position: self.map_location(),
-                map: map,
-                icon: defaultIcon
-            });
-        }
-        return marker;
-    })(self);
-    self.id = ko.observable(dine_spots.id);
-    self.url = ko.observable(dine_spots.url);
-    var url1 = self.url() === undefined ?  '#' : self.url() ;
-    var addr = self.formattedAddress() === undefined ? ' ' : self.formattedAddress();
-    var phone1 = self.formattedPhone() === undefined ? ' ' : self.formattedPhone();
-    console.log(phone1);
-    self.formattedInfoWindowData = function () {
-        return '<div class="info-window-content">' + '<a href="' + url1 + '">' +
-            '<span class="info-window-header"><h4>' + self.name() + '</h4></span>' +
-            '</a><h6>' + addr  + '<br>' +  phone1  + '</h6>' +
-            '</div>';
+            if (self.map_location()) {
+                marker = new google.maps.Marker({
+                    position: self.map_location(),
+                    map: map,
+                    icon: defaultIcon
+                });
+            }
+            return marker;
+        })(self);
+        self.id = ko.observable(dine_spots.id);
+        self.url = ko.observable(dine_spots.url);
+        var url1 = self.url() === undefined ? '#' : self.url();
+        var addr = self.formattedAddress() === undefined ? ' ' : self.formattedAddress();
+        var phone1 = self.formattedPhone() === undefined ? ' ' : self.formattedPhone();
+        console.log(phone1);
+        self.formattedInfoWindowData = function() {
+            return '<div class="info-window-content">' + '<a href="' + url1 + '">' +
+                '<span class="info-window-header"><h4>' + self.name() + '</h4></span>' +
+                '</a><h6>' + addr + '<br>' + phone1 + '</h6>' +
+                '</div>';
+        };
     };
-};
 
 
     //List of dine_spots's after filter based on query added in search
-    self.Filtereddine_spotsList = ko.computed(function () {
-        self.dine_spotsList().forEach(function (dine_spots) {
+    self.filteredDineSpotsList = ko.computed(function() {
+        self.dine_spotsList().forEach(function(dine_spots) {
             dine_spots.marker.setMap(null);
         });
 
-        var results = ko.utils.arrayFilter(self.dine_spotsList(), function (dine_spots) {
+        var results = ko.utils.arrayFilter(self.dine_spotsList(), function(dine_spots) {
             return dine_spots.name().toLowerCase().contains(self.query().toLowerCase());
         });
 
-        results.forEach(function (dine_spots) {
+        results.forEach(function(dine_spots) {
             dine_spots.marker.setMap(map);
         });
         if (results.length > 0) {
-                self.queryResult(results.length + " food spots from Foursquare ");
-            
-        }
-        else {
+            self.queryResult(results.length + " food spots from Foursquare ");
+
+        } else {
             self.queryResult("Sorry no such place found");
         }
         return results;
@@ -112,21 +117,21 @@ var search_Object = function (dine_spots, map) {
     self.queryResult("Loading......");
 
     //function called when a dine_spots is clicked from the filtered list
-    self.selectdine_spots = function (dine_spots) {
+    self.selectdine_spots = function(dine_spots) {
         infoWindow.setContent(dine_spots.formattedInfoWindowData());
         infoWindow.open(map, dine_spots.marker);
         map.panTo(dine_spots.marker.position);
         dine_spots.marker.setAnimation(google.maps.Animation.BOUNCE);
         dine_spots.marker.setIcon(highlightedIcon);
-        self.dine_spotsList().forEach(function (unselected_dine_spots) {
-            if (dine_spots!= unselected_dine_spots) {
+        self.dine_spotsList().forEach(function(unselected_dine_spots) {
+            if (dine_spots != unselected_dine_spots) {
                 unselected_dine_spots.marker.setAnimation(null);
                 unselected_dine_spots.marker.setIcon(defaultIcon);
             }
         });
     };
 
-    
+
     function fetchdine_spots() {
         var data;
 
@@ -135,21 +140,21 @@ var search_Object = function (dine_spots, map) {
             dataType: 'json',
             data: 'client_id=HQ53RPBYCWJQPQXK3OY032MRPYK051ST42UFRSV052ONXBFI&client_secret=1EOZYQPKZPCXURLQRMUS0KIHBKJ0RIPITDQJ5XQPJHB3VU53&v=20161016%20&ll=37.338208,-121.886329%20&query=food',
             async: true,
-        }).done(function (response) {
+        }).done(function(response) {
             data = response.response.venues;
-            data.forEach(function (dine_spots) {
-                object = new search_Object(dine_spots, map);
+            data.forEach(function(dine_spots) {
+                object = new SearchObject(dine_spots, map);
                 self.dine_spotsList.push(object);
             });
-            self.dine_spotsList().forEach(function (dine_spots) {
+            self.dine_spotsList().forEach(function(dine_spots) {
                 if (dine_spots.map_location()) {
-                    google.maps.event.addListener(dine_spots.marker, 'click', function () {
+                    google.maps.event.addListener(dine_spots.marker, 'click', function() {
                         self.selectdine_spots(dine_spots);
                     });
                 }
             });
-        }).fail(function (response, status, error) {
-            $('#query-summary').text('dine_spots could not load...');
+        }).fail(function(response, status, error) {
+            $('#display-query-length').text('Sorry.. food spots could not load due to some network issue...');
         });
     }
 };
@@ -164,6 +169,3 @@ function makeMarkerIcon(mcolor) {
         new google.maps.Size(21, 34));
     return markerImage;
 }
-
-
-
